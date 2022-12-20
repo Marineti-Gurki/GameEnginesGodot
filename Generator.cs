649a26e7411ff87057b73cf41d689001a861dddd
matissolus <matiss2000@inbox.lv>
2022-12-20 03:11:33 +0000

    Added Space Snake
    Buggy
    Want to refine wobble movement
    Need to add path to follow around main game area.

--- Generator.cs
+++ Generator.cs
@@ -8,27 +8,27 @@ public class Generator : Spatial
     int loops = 10;
     public override void _Ready()
     {
-        dodecahedron = (PackedScene)ResourceLoader.Load("res://Dodecahedron.tscn");   
-    int radius = 1;
-    for (int i = 0; i < loops; i++)
-    {
-        float numOfScenes = (float)(2.0f * Mathf.Pi * i);
-        float theta = Mathf.Pi * (2.0f) / (numOfScenes);
-        for (int j = 0; j < numOfScenes; j++)
+        dodecahedron = (PackedScene)ResourceLoader.Load("res://Dodecahedron.tscn");
+        int radius = 1;
+        for (int i = 0; i < loops; i++)
         {
-            float angle = j * theta;
-            float x = (radius) * Mathf.Cos(angle)* i;
-            float y = (radius) * Mathf.Sin(angle)* i;
-            var dodInst = dodecahedron.Instance<MeshInstance>();
-            dodInst.Translation = new Vector3(x, y, 0);
-            AddChild(dodInst);        
+            float numOfScenes = (float)(2.0f * Mathf.Pi * i);
+            float theta = Mathf.Pi * (2.0f) / (numOfScenes);
+            for (int j = 0; j < numOfScenes; j++)
+            {
+                float angle = j * theta;
+                float x = (radius) * Mathf.Cos(angle) * i;
+                float y = (radius) * Mathf.Sin(angle) * i;
+                var dodInst = dodecahedron.Instance<MeshInstance>();
+                dodInst.Translation = new Vector3(x, y, 0);
+                AddChild(dodInst);
+            }
         }
     }
-    }
 
- // Called every frame. 'delta' is the elapsed time since the previous frame.
- public override void _Process(float delta)
- {
-    // Rotation += new Vector3(0, 0, 4);
- }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        // Rotation += new Vector3(0, 0, 4);
+    }
 }
